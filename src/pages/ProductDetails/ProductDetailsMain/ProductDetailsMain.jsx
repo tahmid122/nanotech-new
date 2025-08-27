37be5ad44d9a1e44d81883e7413f5266508ab4b3
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-08-27 15:11:42 +0600

    product details page added but not completed

--- src/pages/ProductDetails/ProductDetailsMain/ProductDetailsMain.jsx
+++ src/pages/ProductDetails/ProductDetailsMain/ProductDetailsMain.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import "./ProductDetailsMain.css";
+import ProductImagesSlider from "./ProductImagesSlider/ProductImagesSlider";
 const ProductDetailsMain = () => {
   return (
     <div id="productDetailsMain">
-      <div className="pdLeft">Product Details</div>
+      <div className="pdLeft">
+        <div className="pdProductImage">
+          <div className="pdProMainImage">
+            <img
+              src="https://cdn.waltonplaza.com.bd/364e7d1d-24dc-4d25-afc8-36ba0aa99322.jpeg"
+              alt="main image"
+            />
+          </div>
+          <div className="pdImagesSlider">
+            <ProductImagesSlider />
+          </div>
+        </div>
+        <div className="pdProductDetails">details</div>
+      </div>
       <div className="pdRight">
-        <div className="deliveryOptions"></div>
+        <div className="deliveryOptions">
+          <div className="doTop">
+            <div className="doTime">
+              <img src="/truck.webp" alt="delivery truck icon" />
+              <span>
+                Home Delivery <br />{" "}
+                <span style={{ fontWeight: "400", fontSize: "14px" }}>
+                  2 - 3 Days
+                </span>
+              </span>
+            </div>
+            <div>৳700</div>
+          </div>
+          <div className="doNotice">
+            <b>Notice:</b> As per the delivery policy, please receive your
+            ordered product within 5 days; otherwise, it will be automatically
+            cancelled.
+          </div>
+          <div className="cashOnDel">
+            <img src="/dollar.webp" alt="dollar" />
+            Cash on Delivery Available
+          </div>
+        </div>
         <div className="returnPolicy">
           <h5>Return & Warranty</h5>
           <hr className="pdHr" />
---
+++ src/pages/ProductDetails/ProductDetailsMain/ProductImagesSlider/ProductImagesSlider.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+const images = [
+  "https://cdn.waltonplaza.com.bd/c18e214d-025c-493d-8aec-e754a3132534.jpeg",
+  "https://cdn.waltonplaza.com.bd/019083d7-fc8f-4902-b208-f1ed09561064.jpeg",
+  "https://cdn.waltonplaza.com.bd/5f4bad83-cc92-4834-9eae-52ef580f4b31.jpeg",
+  "https://cdn.waltonplaza.com.bd/b07300df-63c3-4f0d-98e3-6b3a436d112a.jpeg",
+  "https://cdn.waltonplaza.com.bd/b07300df-63c3-4f0d-98e3-6b3a436d112a.jpeg",
+  "https://cdn.waltonplaza.com.bd/b07300df-63c3-4f0d-98e3-6b3a436d112a.jpeg",
+];
+const ProductImagesSlider = () => {
+  return (
+    <div className="pdSlider">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          // when window width is >= 640px
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {images?.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="slide">
+              <img src={image} alt="image" />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ProductImagesSlider;
