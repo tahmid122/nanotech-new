c5bbea3c7afe8600cd5a9feb276b32ff927660d2
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-09-04 16:28:37 +0600

    Cart page placeholder added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import "./Cart.css";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import Container from "../../components/Container/Container";
 import SalesPointCart from "./SalesPointCart/SalesPointCart";
 import UserCart from "./UserCart/UserCart";
+import useCart from "../../hooks/useCart";
+import Navigation from "../../components/Navigation/Navigation";
+import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 const Cart = () => {
   const navigate = useNavigate();
+  const { cartItems } = useCart();
   const isLoggedIn = true;
   const salesPoint = true;
   useEffect(() => {
@@ -16,7 +20,26 @@ const Cart = () => {
   return (
     <Container>
       <section id="cart">
-        {salesPoint ? <SalesPointCart /> : <UserCart />}
+        {cartItems?.length == 0 ? (
+          <div className="emptyCartPlaceholder">
+            <Navigation links={[{ label: "Home", href: "/" }]} title={"Cart"} />
+            <div className="emptyCartPlaceholderMain">
+              <MdOutlineRemoveShoppingCart
+                size={80}
+                color="gray"
+                opacity={0.5}
+              />
+              <h4> Cart is empty!</h4>
+              <p>
+                Add your desired products to cart and come back to this page to
+                checkout.
+              </p>
+              <Link to={"/"}>Continue Shopping</Link>
+            </div>
+          </div>
+        ) : (
+          <> {salesPoint ? <SalesPointCart /> : <UserCart />}</>
+        )}
       </section>
     </Container>
   );
